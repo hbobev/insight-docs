230b560c954d8984a24fe945262b1bf8b75b294f
Hristiyan Bobev <175059014+hbobev@users.noreply.github.com>
2025-03-15 21:35:39 +0200

    build: initialize data and configs directory structure

---
+++ data/models/document_classifier/model.py
@@ -0,0 +1,5 @@
+"""
+Document classifier model.
+This is a placeholder for a model that will classify documents by type.
+To be implemented later.
+"""
---
+++ data/models/layout_analyzer/model.py
@@ -0,0 +1,5 @@
+"""
+Layout analyzer model.
+This is a placeholder for a model that will analyze document layout.
+To be implemented later.
+"""
