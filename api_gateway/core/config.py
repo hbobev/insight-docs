862a136f91c8276e61fb649393625e2951a6d6b0
Hristiyan Bobev <175059014+hbobev@users.noreply.github.com>
2025-03-19 02:18:15 +0200

    feat: add settings configuration with service-specific parameters and CORS validation

--- api_gateway/core/config.py
+++ api_gateway/core/config.py
@@ -1,7 +1,34 @@
+from pathlib import Path
+
+from pydantic import AnyHttpUrl, field_validator
 from pydantic_settings import BaseSettings
+
+ROOT_DIR = Path(__file__).parents[3]
 
 
 class Settings(BaseSettings):
     """Service-specific settings."""
 
-    pass
+    PROJECT_NAME: str = ""
+    VERSION: str = "0.1.0"
+    API_GATEWAY_HOST: str
+    API_GATEWAY_PORT: int
+    API_GATEWAY_CORS_ORIGINS: list[AnyHttpUrl]
+    DEBUG: bool
+
+    @field_validator("API_GATEWAY_CORS_ORIGINS")
+    def validate_cors_origins(cls, v: list[str]) -> list[AnyHttpUrl] | list[str]:
+        """Validate that CORS origins are valid URLs or use default origins when given as a string."""
+        if isinstance(v, str) and not v.startswith("["):
+            return [AnyHttpUrl(url=origin.strip()) for origin in v.split(",") if origin]
+        if isinstance(v, list):
+            return v
+        raise ValueError(v)
+
+    class Config:
+        case_sensitive = True
+        env_file = str(ROOT_DIR / ".env")
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()
